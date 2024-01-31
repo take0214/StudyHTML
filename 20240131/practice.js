302197fa669c3ea086ad970201a1f5e80df8a52d
rx78take10969@ezweb.ne.jp <82068925+Takehiro0214@users.noreply.github.com>
2024-01-31 22:36:06 +0900

    20240131修正

--- 20240131/practice.js
+++ 20240131/practice.js
@@ -3,7 +3,9 @@ function displayBody () {
     let body = 
     `
     <form>
+    <label>ラジオボタン1</label>
     <input type="radio" name="radio" value="1" onchange="display()" checked>
+    <label>ラジオボタン2</label>
     <input type="radio" name="radio" value="2" onchange="display()">
     </form>
     <div id="displayArea"></div>
@@ -21,13 +23,19 @@ function display(){
             if(radio.value == 1){
                 html = 
                 `
-                <button id="botton01" onclick="displayText('りんご')">ボタン1</button>
-                <button id="botton02" onclick="displayText('みかん')">ボタン2</button>
-                <button id="botton03" onclick="displayText('いちご')">ボタン3</button>
-                <div id="fruit"></div>
+                <button id="botton01" onclick="displayFruit('りんご')">ボタン1</button>
+                <button id="botton02" onclick="displayFruit('みかん')">ボタン2</button>
+                <button id="botton03" onclick="displayFruit('いちご')">ボタン3</button>
+                <h1 id="fruit"></h1>
                 `;
             } else if (radio.value == 2){
-                html = `<h1>AAA</h1>`
+                html =
+                `
+                <h2 id=text>あいうえおかきくけこさしすせそ</h2>
+
+                <input type="text" id="textbox" onchange="displayCharAt()">
+                文字目の文字は"<span id="char">?</span>"です。
+                `;
             }
         }
     }
@@ -35,10 +43,21 @@ function display(){
     document.getElementById("displayArea").innerHTML = html
 }
 
-function displayText (fruit) {
+function displayFruit (fruit) {
     document.getElementById("fruit").innerText = fruit;
 }
 
+function displayCharAt () {
+
+    let text = document.getElementById("text").innerText;
+    let char = text.charAt(Number(document.getElementById("textbox").value) - 1);
+
+    if(char.length == 0){
+        char = "?";
+    }
+
+    document.getElementById("char").innerText = char;
+}
 
 window.onload = function () {
     displayBody();
